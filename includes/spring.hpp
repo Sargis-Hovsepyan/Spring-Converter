8d7fc3e584645d9865a165841207b9e45df7af51
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-04-13 21:51:36 +0400

    The Spring class interface

--- includes/spring.hpp
+++ includes/spring.hpp
@@ -2,9 +2,43 @@
 #define SPRING_HPP
 
 
+#include <vector>
+using std::vector;
+
 class Spring
 {
+    private:
+        double k = 1;
+    
+    public:
+        Spring();
+        Spring(const double k);
+        Spring(const Spring& other);
 
+        Spring&     operator=(const Spring& other);
+
+        void        setK(double k);
+
+        double*     move(double t, double dt, double x0, double v0);
+        double*     move(double t, double dt, double x0);
+
+        double*     move(double t0, double t1, double dt, double x0, double v0);
+        double*     move(double t0, double t1, double dt, double x0, double v0, double m);
+
+        Spring&     in_series(Spring that);
+        Spring&     in_parallel(Spring that);
+
+
+    /* UTILITY METHODS */
+    
+    private:
+        Spring          in_series(const Spring& spring1, const Spring& spring2);
+        Spring          in_parallel(const Spring& spring1, const Spring& spring2);
+
+        vector<double>  get_all_coordinates(double t0, double t1, double dt, double x0, double v0,  double mass);
+
+        double*         list_to_array(vector<double> coordinates);
+        double          harmonic_motion_coordinate(double t, double omega, double c1, double c2);
 };
 
 #endif
