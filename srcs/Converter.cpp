d6eb4cf20a514f7348469c0583cc582256b4c7c4
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-04-30 11:09:32 +0400

    [UPDATE]

--- srcs/Converter.cpp
+++ srcs/Converter.cpp
@@ -9,15 +9,21 @@ Converter::~Converter() {}
 
 double      Converter::convert(const string& bits)
 {
-    int sampleSize = 1024;
+    int sample_size = 1024;
     double t0 = 0;
     double t1 = 2 * PI;
 
     Spring spring = bits_to_spring(bits);
     
-    vector<double>  values = get_oscilations(spring, t0, t1, sampleSize);
+    vector<double>  values = get_oscilations(spring, t0, t1, sample_size);
     vector<Complex> amplitudes = get_amplitudes(values);
-    vector<double>  frequencies = get_frequencies(amplitudes, (t1 - t0) / sampleSize);
+
+    // std::cout << "amplitudes\n";
+    // for (unsigned int i = 0; i < amplitudes.size(); i++)
+    //     std:: cout << amplitudes[i].get_real() << " ";
+    // std::cout << std::endl;
+
+    vector<double>  frequencies = get_frequencies(amplitudes, (t1 - t0) / sample_size);
 
     return freq_to_decimal(amplitudes, frequencies);
 }
@@ -27,18 +33,17 @@ vector<double>      Converter::get_oscilations(Spring& spring, double start, dou
     return spring.move(start, end, end / n, 1, 0, 0.001);
 }
 
-vector<double>      Converter::get_frequencies(const vector<Complex>& amplitudes, double rate)
-{
-    return FT::freq(amplitudes.size(), rate);
-}
-
 vector<Complex>     Converter::get_amplitudes(const vector<double>& values)
 {
-    vector<Complex> amplitudes;
- 
+    vector<Complex> amplitudes; 
     for (unsigned long i = 0; i < values.size(); i++)
         amplitudes.push_back(Complex(values[i], 0.0));
         
     FT::fft(amplitudes);
     return amplitudes;
+}
+
+vector<double>      Converter::get_frequencies(const vector<Complex>& amplitudes, double rate)
+{
+    return FT::freq(amplitudes.size(), rate);
 }
